1f76c23ab28963af1422186c0f6bed76c4ed3bb7
kidus-93 <181005371+kidus-93@users.noreply.github.com>
2026-02-23 00:44:00 +0300

    finding the Maximum number in a list

--- Greeting.py
+++ Greeting.py
@@ -1,28 +1,6 @@
-print("*****- Welcome to python Pizza Deliveries!!! -*****")
-size = input("What size pizza do you want?(S, M, OR L)").upper()
-pepperoni = input("Do you want pepperoni?(Y/N)").upper()
-extra_cheese = input("Do you want extra cheese?(Y/N)").upper()
-bill = 0
-
-if size == "S":
-    bill += 15
-elif size == "M":
-    bill += 20
-elif size == "L":
-    bill += 25
-
-else:
-    print("Please enter a valid size")
-
-if pepperoni == "Y":
-    if size == "S":
-        bill += 2
-    else:
-        bill += 3
-
-if extra_cheese == "Y":
-    bill += 1
-print("Your bill is", bill)
-
-
-
+student_scores = [98,78,47,96,58,89,19,65,100]
+max = 0
+for score in student_scores:
+    if score > max:
+        max = score
+print(max)
