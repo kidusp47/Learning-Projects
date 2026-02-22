9bf996942e9f9ac3d365681e76bdb9d72d0fbb2e
kidus-93 <181005371+kidus-93@users.noreply.github.com>
2026-02-23 00:54:52 +0300

    ranfe Function

--- Greeting.py
+++ Greeting.py
@@ -1,6 +1,4 @@
-student_scores = [98,78,47,96,58,89,19,65,100]
-max = 0
-for score in student_scores:
-    if score > max:
-        max = score
-print(max)
+sum = 0
+for number in range(1,101):
+    sum += number
+print(sum)
