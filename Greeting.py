a76ac163fdfc44345c7a789d2ec702e7cf06a841
kidus-93 <181005371+kidus-93@users.noreply.github.com>
2026-02-22 11:28:42 +0300

    Testing

--- Greeting.py
+++ Greeting.py
@@ -1,3 +1,7 @@
-subcity_name = input("What is your SubCity name? ")
-pet_name = input("What is your Pet name? ")
-print("your bad name is ",subcity_name, pet_name)
+print("""
+1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.
+2. Knead the dough for 10 minutes.
+3. Add 3g of Salt.
+4. Leave to rise for 2 hours.
+5. Bake at 200 degrees C for 30 minutes.
+""")
