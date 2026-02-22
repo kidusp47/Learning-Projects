9ebde2a8985ff943011e91cfa5e1383a47e04403
kidus-93 <181005371+kidus-93@users.noreply.github.com>
2026-02-22 13:07:30 +0300

    Improved ticket stand

--- Greeting.py
+++ Greeting.py
@@ -1,11 +1,23 @@
-weight = float(input("Enter your weight(kg): "))
-height = float(input("Enter your height(m): "))
+print("*****- Disneyland ride ticket stand -*****")
+name = input("what is your name? ")
+age = int(input("what is your age? "))
+height = float(input("what is your height?(cm) "))
+bill = 0
 
-bmi = weight / (height ** 2)
 
-if bmi < 18.5:
-    print("You are under weight")
-elif bmi >= 18.5 and bmi <= 24.5:
-    print("You are normal Weight")
+if height >= 120:
+    if age <12 :
+        bill = bill + 5
+    elif 12 <= age <= 18:
+        bill = bill + 7
+    else:
+        bill = bill + 12
+
+    photo = input("would you like a photo(y/n)")
+    if photo == "y":
+        bill = bill + 3
+    print("your bill is", bill)
+
 else:
-    print("You are over weight")
+    print(f"sorry {name}, you are too short")
+
