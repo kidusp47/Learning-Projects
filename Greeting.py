7fb16dc4dcadcb955b28def65ea02db8ecbad97f
kidus-93 <181005371+kidus-93@users.noreply.github.com>
2026-02-22 12:41:32 +0300

    Improved BMI Calculator

--- Greeting.py
+++ Greeting.py
@@ -1,5 +1,11 @@
-print("***BMI Calculator***")
-height = float(input("Height(m): "))
-weight = float(input("Weight(Kg): "))
-bmi = round(weight / (height**2),2)
-print(f"Your BMI is: {bmi}")
+weight = float(input("Enter your weight(kg): "))
+height = float(input("Enter your height(m): "))
+
+bmi = weight / (height ** 2)
+
+if bmi < 18.5:
+    print("You are under weight")
+elif bmi >= 18.5 and bmi <= 24.5:
+    print("You are normal Weight")
+else:
+    print("You are over weight")
