2bb0ac7b4628235737fbf50571588133c9c54277
kidus-93 <181005371+kidus-93@users.noreply.github.com>
2026-02-25 11:35:11 +0300

    Dictionaries

--- Greeting.py
+++ Greeting.py
@@ -1,5 +1,16 @@
-def greeting():
-     print("Hello")
-     print("bye")
+programing_dictionary = {
+    "Bug": "an error in a program that prevents the program from running as expected ",
+    "Function": "A piece of code that you can easily call over and over again",
+    "Loop": "The action of doing something over and over again"
+    }
+# print(programing_dictionary["Function"])
+# programing_dictionary["Loop"] = "The action of doing something over and over again "
+# print(programing_dictionary)
+#
+# empty_dictionary = {}
+# programing_dictionary={}
+# print(programing_dictionary)
 
-greeting()
+for key in programing_dictionary:
+    print(key, programing_dictionary[key])
+    print()
